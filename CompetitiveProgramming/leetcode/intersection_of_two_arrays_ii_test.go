9792f9a0eca2eed4e4131a34d73c7a99004e62e6
Vicktor Desrony <vldcreation21@gmail.com>
2024-06-11 19:54:24 +0700

    add test intersect of two arrays

---
+++ CompetitiveProgramming/leetcode/intersection_of_two_arrays_ii_test.go
@@ -0,0 +1,57 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/vldcration/go-ressources/util"
+)
+
+func TestIntersect(t *testing.T) {
+	tests := []struct {
+		name     string
+		nums1    []int
+		nums2    []int
+		expected [][]int
+	}{
+		{
+			name:  "TC1",
+			nums1: []int{1, 2, 2, 1},
+			nums2: []int{2, 2},
+			expected: [][]int{
+				{2, 2},
+			},
+		},
+		{
+			name:  "TC2",
+			nums1: []int{4, 9, 5},
+			nums2: []int{9, 4, 9, 8, 4},
+			expected: [][]int{
+				{4, 9},
+			},
+		},
+		{
+			name:  "TC3",
+			nums1: []int{0, 2, 7, 7, 9},
+			nums2: []int{0, 8, 4},
+			expected: [][]int{
+				{4, 9},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		result := intersect(tc.nums1, tc.nums2)
+
+		ok := false
+		for _, v := range tc.expected {
+			if util.CompareSliceInt(result, v) {
+				ok = true
+				break
+			}
+		}
+
+		if !ok {
+			t.Errorf("%s failed, Expected one of %v, got %v", tc.name, tc.expected, result)
+		}
+	}
+}
