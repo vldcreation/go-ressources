a16904e8beaebcef3d180ef606452d5f35be3bcf
Vicktor Desrony <vldcreation21@gmail.com>
2025-01-11 17:45:14 +0700

    solve(max-consecutive-ones): solve problem

--- CompetitiveProgramming/leetcode/problem/easy/max-consecutive-ones.go
+++ CompetitiveProgramming/leetcode/problem/easy/max-consecutive-ones.go
@@ -1,5 +1,34 @@
 package easy
 
+// 1, 1, 0, 1, 1, 1
+// return 3
+// 1, 0, 1, 1, 0, 1
+// return 2
 func FindMaxConsecutiveOnes(nums []int) int {
-	return 0
+	ans, count := 0, 0
+	for _, num := range nums {
+		if num == 1 {
+			count++
+		} else {
+			ans = max(ans, count)
+			count = 0
+		}
+	}
+	return max(ans, count)
+}
+
+func FindMaxConsecutiveOnesV2(nums []int) int {
+	left, right, maxCount := 0, 0, 0
+
+	for right < len(nums) {
+		if nums[right] == 1 {
+			right++
+		} else {
+			maxCount = max(maxCount, right-left)
+			right++
+			left = right
+		}
+	}
+	maxCount = max(maxCount, right-left)
+	return maxCount
 }
