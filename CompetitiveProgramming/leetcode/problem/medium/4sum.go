84fa38031601737d5cee3e599e8a4db8a7981d97
Vicktor Desrony <vldcreation21@gmail.com>
2025-01-11 11:20:21 +0700

    solve(4sum): solve 4 sum

--- CompetitiveProgramming/leetcode/problem/medium/4sum.go
+++ CompetitiveProgramming/leetcode/problem/medium/4sum.go
@@ -1,6 +1,42 @@
 package medium
 
+import "sort"
+
 func FourSum(nums []int, target int) [][]int {
-	ans := make([][]int, 0)
-	return ans
+	var result [][]int
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue // skip duplicate elements
+		}
+
+		for j := i + 1; j < len(nums)-1; j++ {
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue // skip duplicate elements
+			}
+
+			left, right := j+1, len(nums)-1
+			for left < right {
+				sum := nums[i] + nums[j] + nums[left] + nums[right]
+				if sum == target {
+					result = append(result, []int{nums[i], nums[j], nums[left], nums[right]})
+					for left < right && nums[left] == nums[left+1] {
+						left++ // skip duplicate elements
+					}
+					for left < right && nums[right] == nums[right-1] {
+						right-- // skip duplicate elements
+					}
+					left++
+					right--
+				} else if sum < target {
+					left++
+				} else {
+					right--
+				}
+			}
+		}
+	}
+
+	return result
 }
