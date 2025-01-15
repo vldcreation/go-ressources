5f441348638aab41785a94df881602a29f1ce3c5
Vicktor Desrony <vldcreation21@gmail.com>
2025-01-15 16:55:19 +0700

    solve(RemoveDuplicatesFromSortedArray): add add test case

--- CompetitiveProgramming/leetcode/problem/easy/problem_test.go
+++ CompetitiveProgramming/leetcode/problem/easy/problem_test.go
@@ -30,3 +30,28 @@ func TestFindMaxConsecutiveOnesI(t *testing.T) {
 		}
 	}
 }
+
+func TestRemoveDuplicatesFromSortedArray(t *testing.T) {
+	tests := []struct {
+		name     string
+		nums     []int
+		expected int
+	}{
+		{
+			name:     "Test1",
+			nums:     []int{1, 1, 2},
+			expected: 2,
+		},
+		{
+			name:     "Test1",
+			nums:     []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+			expected: 5,
+		},
+	}
+
+	for _, tt := range tests {
+		if got := easy.RemoveDuplicatesFromSortedArray(tt.nums); got != tt.expected {
+			t.Errorf("failed on test (%s): Expected %v, but got %v", tt.name, tt.expected, got)
+		}
+	}
+}
