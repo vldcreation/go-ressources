83eef9f5c4dc12a935909bac380ea103cf95c06b
Vicktor Desronyclear <vldcreation21@gmail.com>
2025-01-23 12:52:34 +0700

    solve(hackerrank): add test case for FormingMagicSquare

---
+++ CompetitiveProgramming/hackerrank/problem/medium/problem_test.go
@@ -0,0 +1,32 @@
+package medium_test
+
+import (
+	"testing"
+
+	"github.com/vldcration/go-ressources/CompetitiveProgramming/hackerrank/problem/medium"
+)
+
+func TestMergeTwoLists(t *testing.T) {
+	tests := []struct {
+		name     string
+		nums     [][]int32
+		expected int32
+	}{
+		{
+			name:     "Test1",
+			nums:     [][]int32{{4, 9, 2}, {3, 5, 7}, {8, 1, 5}},
+			expected: 1,
+		},
+		{
+			name:     "Test1",
+			nums:     [][]int32{{4, 8, 2}, {4, 5, 7}, {6, 1, 6}},
+			expected: 4,
+		},
+	}
+
+	for _, tt := range tests {
+		if got := medium.FormingMagicSquare(tt.nums); got != tt.expected {
+			t.Errorf("failed on test (%s): Expected %v, but got %v", tt.name, tt.expected, got)
+		}
+	}
+}
