da937f84b0ac9b86312de3157242b42523edd763
Vicktor Desrony <vldcreation21@gmail.com>
2025-02-13 11:10:50 +0700

    interview: refactor bh solution, reduce memory usage

--- CompetitiveProgramming/interview/bosshire/main/main.go
+++ CompetitiveProgramming/interview/bosshire/main/main.go
@@ -1,35 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
+// n = 5
 // 54321
 // 5432
 // 543
 // 543
 // 54
 // 5
+// n = 6
+// 654321
+// 65432
+// 6543
+// 654
+// 65
+// 6
 func main() {
-	n := 6
-	for i := 1; i <= n; i++ {
+	if len(os.Args) < 1 {
+		panic("Please pass n number")
+	}
+	n, err := strconv.ParseInt(os.Args[1], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := int64(1); i <= n; i++ {
 		if n%2 == 1 {
-			// fmt.Printf("I: %d, n/2L %d ", i, n/2)
 			if i == (n/2)+1 {
 				for j := n; j >= i; j-- {
 					fmt.Printf("%d", j)
 				}
 				println()
-				for j := n; j >= i; j-- {
-					fmt.Printf("%d", j)
-				}
-			} else {
-				for j := n; j >= i; j-- {
-					fmt.Printf("%d", j)
-				}
 			}
-		} else {
-			for j := n; j >= i; j-- {
-				fmt.Printf("%d", j)
-			}
+		}
+		for j := n; j >= i; j-- {
+			fmt.Printf("%d", j)
 		}
 		fmt.Println()
 	}
