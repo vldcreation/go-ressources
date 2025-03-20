681fc0fe5e80b780948816c2e56ba12a7a64a6f8
Vicktor Desrony <vldcreation21@gmail.com>
2025-03-20 16:25:43 +0700

    feat: move test longest palindrom to medium package

--- CompetitiveProgramming/leetcode/problem/problem_test.go
+++ CompetitiveProgramming/leetcode/problem/problem_test.go
@@ -175,38 +175,6 @@ func TestZigzagConversion(t *testing.T) {
 	}
 }
 
-func TestLongestPalindrome(t *testing.T) {
-	tests := []struct {
-		name     string
-		str      string
-		expected []string
-	}{
-		{
-			name:     "Test 1",
-			str:      "babad",
-			expected: []string{"bab", "aba"},
-		},
-		{
-			name:     "Test 2",
-			str:      "cbbd",
-			expected: []string{"bb"},
-		},
-		{
-			name:     "Test 3",
-			str:      "ccc",
-			expected: []string{"ccc"},
-		},
-	}
-
-	for _, tc := range tests {
-		result := medium.LongestPalindrome(tc.str)
-
-		if !util.CheckStringInSlice(result, tc.expected) {
-			t.Errorf("%s failed, Expected one of %v, got %s", tc.name, tc.expected, result)
-		}
-	}
-}
-
 func TestIntersect(t *testing.T) {
 	tests := []struct {
 		name     string
