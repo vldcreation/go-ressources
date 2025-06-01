f3ba6f4730e017450e3d0416eb433ea8e5adb5ce
Vicktor Desrony <vldcreation21@gmail.com>
2025-06-01 17:54:17 +0700

    refactor: optimize CountGoodSubstrings function by removing nested loops and unnecessary checks

--- CompetitiveProgramming/leetcode/problem/easy/1876.go
+++ CompetitiveProgramming/leetcode/problem/easy/1876.go
@@ -1,25 +1,12 @@
 package easy
 
-import "fmt"
-
 func CountGoodSubstrings(s string) int {
 	ans := 0
-
-	for i := 0; i < len(s); i++ {
-		for j := i + 1; j < len(s); j++ {
-			for k := j + 1; k < len(s); k++ {
-				// fmt.Println(i, j, k)
-				if ok(string(s[i]) + string(s[j]) + string(s[k])) {
-					ans++
-				}
-			}
+	for i := 2; i < len(s); i++ {
+		if s[i] != s[i-1] && s[i-1] != s[i-2] && s[i] != s[i-2] {
+			ans++
 		}
 	}
 
 	return ans
-}
-
-func ok(s string) bool {
-	fmt.Println(s)
-	return s[0] != s[1] && s[1] != s[2] && s[0] != s[2]
 }
